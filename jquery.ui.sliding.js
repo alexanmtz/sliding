595a90c83ae059bb9cfcf4234feda9036b51073b
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2011-05-17 18:56:43 -0300

    add support for jQuery ui classes and theme roller

--- jquery.ui.sliding.js
+++ jquery.ui.sliding.js
@@ -22,6 +22,8 @@ $.widget( "ui.sliding", {
      containerSize = parseInt($(this.element).find(this.options.item).css('width')) * (this.options.itens);
      overallSize = parseInt($(this.element).find(this.options.item).css('width')) * ($(this.options.item, this.element).length);
      
+     $(this.element).addClass('ui-widget ui-widget-content ui-corner-all ui-sliding-content');
+     
      if(this.options.mode == 'horizontal') {
         $(this.element).find(this.options.item).css('float','left');
         $(this.element).css({
