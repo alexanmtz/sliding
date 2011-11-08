2eceebd4ff72719b5c2bd7b6ecb3534e0aa8f129
Alexandre Magno <alexanmtz@gmail.com>
2011-11-08 17:29:47 -0200

    fixing the example with ajax that not start correctly

--- json.php
+++ json.php
@@ -1,5 +1,1 @@
-<?php header('Content-type: application/json'); ?>
-[{
-  "pages": "5",
-  "content": "<li>json item 01</li><li>json item 02</li><li>json item 03</li>"
-}]
+<li>json item 01</li><li>json item 02</li><li>json item 03</li>
