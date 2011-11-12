62aa261aea4b88de88e4d58f205c2878024588e9
Alexandre Magno <alexanmtz@gmail.com>
2011-11-12 02:28:36 -0200

    integrating examples in github page

---
+++ ajax.php
@@ -0,0 +1,9 @@
+<li>
+	<img src="thumbs/image01.jpg" width="200" height="200" />
+</li>
+<li>
+	<img src="thumbs/image02.jpg" width="200" height="200" />
+</li>
+<li>
+	<img src="thumbs/image03.jpg" width="200" height="200" />
+</li>
